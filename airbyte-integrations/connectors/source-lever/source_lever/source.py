3b7bfbee7da5e759e3d833ef463838052c7daf9c
Marx <marx@knoetic.com>
2023-10-19 21:16:01 +0800

    Complete incremental stream

--- airbyte-integrations/connectors/source-lever/source_lever/source.py
+++ airbyte-integrations/connectors/source-lever/source_lever/source.py
@@ -6,10 +6,11 @@
 from abc import ABC
 from typing import Any, Iterable, List, Mapping, MutableMapping, Optional, Tuple
 
+import pendulum
 import requests
 from airbyte_cdk.models import SyncMode
 from airbyte_cdk.sources import AbstractSource
-from airbyte_cdk.sources.streams import Stream
+from airbyte_cdk.sources.streams import Stream, IncrementalMixin
 from airbyte_cdk.sources.streams.http import HttpStream
 from airbyte_cdk.sources.streams.http.auth import BasicHttpAuthenticator
 
@@ -36,25 +37,31 @@ class SourceLever(AbstractSource):
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         authenticator = _auth_from_config(config)
-        # TODO: use extract_start_date
+
+        incremental_stream_params = {
+            "authenticator":authenticator, 
+            "start_date": '1990-01-01'
+        }
+
+        opportunity_steam = Opportunities(**incremental_stream_params)
+
         return [
-            Opportunities(authenticator=authenticator),
-            Offers(authenticator=authenticator),
-            Feedback(authenticator=authenticator),
-            Interviews(authenticator=authenticator),
+            opportunity_steam,
+            Offers(**incremental_stream_params, parent_stream=opportunity_steam),
+            Feedback(**incremental_stream_params, parent_stream=opportunity_steam),
+            Interviews(**incremental_stream_params, parent_stream=opportunity_steam),
+            Notes(**incremental_stream_params, parent_stream=opportunity_steam),
+            Panels(**incremental_stream_params, parent_stream=opportunity_steam),
             Requisitions(authenticator=authenticator),
             Users(authenticator=authenticator),
             Stages(authenticator=authenticator),
             Postings(authenticator=authenticator),
             ArchiveReasons(authenticator=authenticator),
-            Panels(authenticator=authenticator),
             Tags(authenticator=authenticator),
             Sources(authenticator=authenticator),
             RequisitionFields(authenticator=authenticator),
-            Notes(authenticator=authenticator)
         ]
 
-# Basic full refresh stream
 class LeverStream(HttpStream, ABC):
     page_size = 100
     stream_params = {}
@@ -73,7 +80,7 @@ class LeverStream(HttpStream, ABC):
             return {"offset": response_data["next"]}
 
     def request_params(
-        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
+        self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
     ) -> MutableMapping[str, Any]:
         params = {"limit": self.page_size}
         params.update(self.stream_params)
@@ -84,14 +91,97 @@ class LeverStream(HttpStream, ABC):
     def parse_response(self, response: requests.Response, stream_slice:Mapping[str, Any],  **kwargs) -> Iterable[Mapping]:
         yield from response.json()["data"]
 
-class Opportunities(LeverStream):
-    primary_key = "id"
-    stream_params = {"confidentiality": "all", "expand": "contact", "expand": "applications"}
-    
+"""
+TODO: Temp workaround.
 
+For incremental substreams to work, make sure the connector is set up with the state:
+[
+  {
+    "streamDescriptor": {
+      "name": "opportunities"
+    },
+    "streamState": {
+      "updatedAt": 631152000000
+    }
+  }
+]
+
+There is an issue where an empty state {} will cause the parent Opportunity stream to update state wrongly.
+The state after the first run of an incremental stream will become: 
+[
+  {
+    "streamDescriptor": {
+      "name": "opportunities"
+    },
+    "streamState": {
+      "updatedAt": "None"
+    }
+  }
+]
+
+This breaks the implementation we have below since we **reasonably** assume `updatedAt: int`. By setting the initial state
+to a valid `updatedAt: number` value, we can work around this issue and subsequent updates will work correctly using the Opportunity's latest record
+"""
+# 
+# 
+class IncrementalLeverStream(LeverStream, IncrementalMixin):
+    state_checkpoint_interval = 100
+    cursor_field = "updatedAt"
+
+    def __init__(self, start_date: str, **kwargs):
+        super().__init__(**kwargs)
+        self._start_timestamp = int(pendulum.parse(start_date).timestamp()) * 1000
+        self._cursor_value = None
+        self._initial_cursor_value = None
+    
     @property
-    def use_cache(self) -> bool:
-        return True
+    def state(self) -> Mapping[str, Any]:
+        return {self.cursor_field: str(self._cursor_value)}
+
+    @state.setter
+    def state(self, value: Mapping[str, Any]):
+        if value[self.cursor_field] and value[self.cursor_field] != 'None' :
+            self._cursor_value = value[self.cursor_field]
+            if not self._initial_cursor_value:
+                self._initial_cursor_value = value[self.cursor_field]
+        else: 
+            self._cursor_value = self._start_timestamp
+            self._initial_cursor_value = self._start_timestamp
+
+    """
+    We use this function so sub streams can reference the inital state the parent stream started with e.g:
+    - initial cursor value = 2023-01-01
+    - after stream completes, cursor value = now()
+    substream run should use initial cursor value(2023-01-01). Using now() will give us too little records
+    """
+    def initial_state(self) -> Mapping[str, Any]:
+        return {self.cursor_field: self._initial_cursor_value}
+
+    def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
+        for record in super().read_records(*args, **kwargs):
+            if self._cursor_value:
+                latest_record_date = record[self.cursor_field]
+                self._cursor_value = max(int(self._cursor_value), int(latest_record_date))
+            yield record
+
+    
+    def request_params(self, stream_state: Mapping[str, Any] = None, **kwargs):
+        stream_state = stream_state or {}
+        params = super().request_params(stream_state=stream_state, **kwargs)
+        stream_state_timestamp = stream_state.get(self.cursor_field, 0) or 0
+        params["updated_at_start"] = max(int(stream_state_timestamp), self._start_timestamp)
+        print("> Request params:", params)
+
+        return params
+
+class Opportunities(IncrementalLeverStream):
+    """
+    Opportunities stream: https://hire.lever.co/developer/documentation#opportunities
+    """
+
+    use_cache = True
+    primary_key = "id"
+    stream_params = {"confidentiality": "all", "expand": ["contact", "applications"], "stage_id": "offer"}
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
@@ -99,6 +189,7 @@ class Opportunities(LeverStream):
         return "opportunities"
 
 class Requisitions(LeverStream):
+    # https://hire.lever.co/developer/documentation#requisitions
     primary_key = "id"
 
     def path(
@@ -107,6 +198,9 @@ class Requisitions(LeverStream):
         return "requisitions"  
 
 class Users(LeverStream):
+    """
+    Users stream: https://hire.lever.co/developer/documentation#users
+    """
     primary_key = "id"
 
     def path(
@@ -115,6 +209,9 @@ class Users(LeverStream):
         return "users"
 
 class Stages(LeverStream):
+    """
+    Stages stream: https://hire.lever.co/developer/documentation#stages
+    """
     primary_key = "id"
 
     def path(
@@ -123,6 +220,9 @@ class Stages(LeverStream):
         return "stages"
     
 class Postings(LeverStream):
+    """
+    Postings stream: https://hire.lever.co/developer/documentation#postings
+    """
     primary_key = "id"
 
     def path(
@@ -131,6 +231,9 @@ class Postings(LeverStream):
         return "postings"
 
 class Tags(LeverStream):
+    """
+    Tags stream: https://hire.lever.co/developer/documentation#tags
+    """
     primary_key = "text"
 
     def path(
@@ -139,6 +242,9 @@ class Tags(LeverStream):
         return "tags"
 
 class Sources(LeverStream):
+    """
+    Sources stream: https://hire.lever.co/developer/documentation#sources
+    """
     primary_key = "text"
 
     def path(
@@ -147,6 +253,9 @@ class Sources(LeverStream):
         return "sources"
     
 class RequisitionFields(LeverStream):
+    """
+    Requisiton fields stream: https://hire.lever.co/developer/documentation#requisition-fields
+    """
     primary_key = "text"
 
     def path(
@@ -154,8 +263,10 @@ class RequisitionFields(LeverStream):
     ) -> str:
         return "requisition_fields"
 
-
 class ArchiveReasons(LeverStream):
+    """
+    Archive Reasons stream: https://hire.lever.co/developer/documentation#archive-reasons
+    """
     primary_key = "id"
 
     def path(
@@ -164,60 +275,35 @@ class ArchiveReasons(LeverStream):
         return "archive_reasons"
 
 
-# TODO: Basic incremental stream
-class IncrementalLeverStream(LeverStream, ABC):
-    """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
-    """
-
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
-
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
 # Basic Sub streams using Opportunity id
 class OpportunitySubStream(LeverStream, ABC):
-    def __init__(self, **kwargs):
+
+    def __init__(self, start_date:str, parent_stream:IncrementalLeverStream, **kwargs):
         super().__init__(**kwargs)
-        
-    # def __init__(self, start_date: str, **kwargs):
-    #     super().__init__(**kwargs)
-    #     self._start_date = start_date
+        self._start_date = start_date
+        self.parent_stream = parent_stream
+
     
-    def path(self, stream_slice: Mapping[str, any] = None, **kwargs) -> str:
+    
+    def path(self, stream_slice: Mapping[str, Any] = None, **kwargs) -> str:
         return f"opportunities/{stream_slice['opportunity_id']}/{self.name}"
 
     def stream_slices(self, **kwargs) -> Iterable[Optional[Mapping[str, Any]]]:
         for stream_slice in super().stream_slices(**kwargs):
-            # opportunities_stream = Opportunities(authenticator=self.authenticator, base_url=self.base_url, start_date=self._start_date)
-            opportunities_stream = Opportunities(authenticator=self.authenticator)
-            for opportunity in opportunities_stream.read_records(sync_mode=SyncMode.full_refresh, stream_slice=stream_slice):
+            for opportunity in self.parent_stream.read_records(stream_state=self.parent_stream.initial_state(), stream_slice=stream_slice, sync_mode=SyncMode.incremental):
                 yield {"opportunity_id": opportunity["id"]}
     
 
     def parse_response(self, response: requests.Response, stream_slice:[Mapping[str, Any]], **kwargs) -> Iterable[Mapping]:
         records = response.json()["data"]
+
+        # https://airbytehq.slack.com/archives/C027KKE4BCZ/p1696509193002769
+        # Fixes the issue where an empty array returned was not refreshing our heartbeat and causing a timeout issue.
+        # if records = [], we will add an empty object in the array
         if not records:
             records = [{}]
         
+        # Adds the parent stream's ID in each substream record
         for record in records:
             record["opportunity"] = stream_slice["opportunity_id"]
         yield from records
